00bf1baafca0ee9b2029f4a8e2f164adb0adb207
John McDonald <jmcdonald1991@gmail.com>
2021-01-17 16:50:52 -0600

    added landing page for api (better than blank page)

---
+++ server/server.js
@@ -0,0 +1,12 @@
+require("dotenv");
+
+const express = require("express");
+const app = express();
+
+app.get("/", (req, res) => {
+  res.sendFile(path.join(__dirname + "./pages/landing.html"));
+});
+
+app.listen(process.env.PORT || 3000, () => {
+  console.log(`App listening at http://localhost:${process.env.PORT || 3000}`);
+});
