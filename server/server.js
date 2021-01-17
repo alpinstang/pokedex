9e05e730fc3b2183a673b8462bcabdfe9bb6e8cb
John McDonald <jmcdonald1991@gmail.com>
2021-01-17 17:48:54 -0600

    landing page styles

--- server/server.js
+++ server/server.js
@@ -4,7 +4,7 @@ const express = require("express");
 const app = express();
 
 app.get("/", (req, res) => {
-  res.sendFile(path.join(__dirname + "./pages/landing.html"));
+  res.sendFile(__dirname + "/pages/landing.html");
 });
 
 app.listen(process.env.PORT || 3000, () => {
