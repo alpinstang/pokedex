50c9532697ef35b8f4923a1e8e0bb70de73ce6c5
John McDonald <jmcdonald1991@gmail.com>
2021-01-17 23:17:01 -0600

    test suite

--- server/test/test.js
+++ server/test/test.js
@@ -2,8 +2,16 @@ const request = require("supertest");
 const assert = require("assert");
 const app = require("../app");
 
-describe("GET /", function () {
-  it("responds with 200", function (done) {
-    request(app).get("/").expect(200, done);
+describe("Test server is running", function () {
+  afterAll(async (done) => {
+    await app.close();
+
+    app.server.stop();
+    done();
+  });
+
+  it("GET / responds with 200", async () => {
+    const result = await app.get("/");
+    expect(result.status).toEqual(200);
   });
 });
