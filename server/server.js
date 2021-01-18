ba203a5ed61c451a1b2b3e57a894a76ede159303
John McDonald <jmcdonald1991@gmail.com>
2021-01-17 20:11:11 -0600

    styling, added links and attributions

--- server/server.js
+++ server/server.js
@@ -4,7 +4,7 @@ const express = require("express");
 const app = express();
 
 app.get("/", (req, res) => {
-  res.sendFile(__dirname + "/pages/landing.html");
+  res.sendFile(__dirname + "/pages/index.html");
 });
 
 app.listen(process.env.PORT || 3000, () => {
