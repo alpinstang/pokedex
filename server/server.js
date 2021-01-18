11259d44252c1e6efaea9268e245a72fcd4adc7e
John McDonald <jmcdonald1991@gmail.com>
2021-01-17 20:34:31 -0600

    pokemon data endpoint

--- server/server.js
+++ server/server.js
@@ -1,12 +1,3 @@
 require("dotenv");
-
 const express = require("express");
 const app = express();
-
-app.get("/", (req, res) => {
-  res.sendFile(__dirname + "/pages/index.html");
-});
-
-app.listen(process.env.PORT || 3000, () => {
-  console.log(`App listening at http://localhost:${process.env.PORT || 3000}`);
-});
